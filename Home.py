e662cfba0c41bdce4208b2b3f611ee653e2296fb
Palak Bansal <bansalpalak774@gmail.com>
2025-06-29 12:50:14 +0530

    Initial commit

---
+++ Home.py
@@ -0,0 +1,91 @@
+import streamlit as st
+# from pages.Price_Prediction import Price_Prediction_Page
+# from pages.Recommendation_System import Recommendation_System_Page
+# from pages.Loan_Eligibility import load_eligibility_Page
+
+# Set page configuration
+st.set_page_config(
+    page_title="HomeQuest.AI",
+    page_icon="🏠",
+    layout="wide",
+)
+
+st.markdown(
+    """
+        <style>
+               .block-container {
+                    padding-top: 2rem;
+                    padding-bottom: 0rem;
+                    # padding-left: 2rem;
+                    # padding-right:2rem;
+                }
+                .top-margin{
+                    margin-top: 4rem;
+                    margin-bottom:2rem;
+                }
+                .block-button{
+                    padding: 10px; 
+                    width: 100%;
+                    background-color: #c4fcce;
+                }
+        </style>
+        """,
+    unsafe_allow_html=True,
+)
+
+
+# Main page function
+def main_page():
+    Overview_col, Img_col = st.columns(spec=(1.4, 1), gap="large")
+
+    with Overview_col:
+
+        # Content for main page
+        st.markdown(
+            "<h1 style='text-align: left; font-size: 70px; '>HomeQuest.AI🏡</h1>",
+            unsafe_allow_html=True,
+        )
+        st.write("")
+        st.markdown("""
+            <p style='font-size: 23px; text-align: left;'>
+                The AI-Powered Home Finder revolutionizes Gurgaon's house-hunting with sophisticated AI algorithms. Seamlessly streamlining the process, it offers tailored recommendations based on individual preferences and needs. Three integrated modules enhance the user experience.
+            </p>
+        """, unsafe_allow_html=True)
+
+        st.write("")
+        st.markdown("""
+            <div>
+                <ul>
+                    <li><p style='font-size: 22px; text-align: left;'><strong><em>The Data Analysis Module:</em></strong> Harnessing comprehensive data analytics, this module uncovers valuable insights from real estate datasets, enabling informed decision-making with actionable intelligence.</p></li>
+                    <li><p style='font-size: 22px; text-align: left;'><strong><em>The Price Prediction Module:</em></strong> Employs machine learning to deliver precise property value estimations based on user preferences, facilitating confident real estate decisions.</p></li>
+                    <li><p style='font-size: 22px; text-align: left;'><strong><em>The Regression Analysis Module:</em></strong> Utilizes advanced regression models to identify key factors influencing property prices, enabling accurate predictions accounting for complex data relationships.</p></li>
+                    <li><p style='font-size: 22px; text-align: left;'><strong><em>The Loan Eligibility Module:</em></strong> Seamlessly integrated, assesses user eligibility for loans by analyzing financial profiles, aiding in effective financial planning for real estate transactions.</p></li>
+                    <li><p style='font-size: 22px; text-align: left;'><strong><em>The Recommendation System Module:</em></strong> Utilizes advanced recommendation algorithms to match user preferences with tailored property suggestions, streamlining the property search process with personalized recommendations.</p></li>
+                </ul>
+            </div>
+        """, unsafe_allow_html=True)
+
+    with Img_col:
+        st.write("")
+        st.markdown("<div class='top-margin'> </div>", unsafe_allow_html=True)
+        st.image("Artifacts/Banner.jpg")
+        st.write("")
+
+        social_col1, social_col2 = st.columns(spec=(1, 1), gap="large")
+        with social_col1:
+            st.link_button("Github👨‍💻", use_container_width=True, url="https://github.com/palakbansa05")
+
+        with social_col2:
+            st.link_button("Linkedin🧑‍💼",use_container_width=True,url = "https://www.linkedin.com/in/palak-bansal-58b845339/")
+
+
+
+# page_names_to_funcs = {
+#     "Project Overview": main_page,
+#     "Property Price Prediction🗃️": Price_Prediction_Page,
+#     "Recommendation Engine": Recommendation_System_Page,
+#     "Loan Eligibility Module": load_eligibility_Page,
+# }
+# selected_page = st.sidebar.selectbox("Select Module", list(page_names_to_funcs.keys()))
+# page_names_to_funcs[selected_page]()
+main_page()
---
+++ README.md
@@ -0,0 +1,11 @@
+# HomeQuest.AI 🏠
+The AI-Powered Home Finder is a groundbreaking solution revolutionizing the home search experience in Gurgaon. Utilizing advanced AI algorithms, it delivers personalized recommendations based on individual preferences and requirements. Users can input specific details such as price range and preferred amenities to receive tailored suggestions. The platform combines Facilities-based and Price-based recommendation engines, allowing users to customize weighting for optimal results. Additionally, the HomeLoan Assurance Advisor provides invaluable insights into loan eligibility, ensuring informed decision-making for property investments. Welcome to the future of home hunting with precision and convenience.
+
+
+If you want to interact with the application by yourself then checkout this ➡️ [Live Project Link](https://findhome-kblfvc5xluhuzswmgnv9fg.streamlit.app/)
+
+
+Prior to accessing the application via the provided link, please take note of the following instructions⚠️
+- Change the application theme from dark to light by navigating to the settings option located at the top left corner upon launching the application.
+- Optimize the screen zoom to 80% to ensure an optimal user experience.
+
